809448cd0bf927c4755fbc52167c5f37db15e2fe
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-12 00:13:15 +0200

    commit

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ require('express-async-errors');
 
 const express = require('express');
 const app = express();
-
+const routerLogin = require('./routes/main')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
 app.use(express.static('./public'));
 app.use(express.json());
+
+app.use('/api/v1',routerLogin)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
