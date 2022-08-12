3323915a562a1ce0994436201ccf66b1a2200042
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-12 03:17:40 +0200

    commit

--- controllers/main.js
+++ controllers/main.js
@@ -4,15 +4,13 @@ const jwt = require('jsonwebtoken');
 
 const login=async (req,res)=>{
     const {username,password}=req.body
+    const id = String(Date.now())
     //check if the login/register variable are there first using our own class CustomAPIError
     if (!username || !password){
         throw new CustomAPIError('please provide your name and password',400)
     }
-    const id =Date.now()
-    console.log(id)
-    const token= jwt.sign({username,id},process.env.JWT_SECRET,{expiresIN:'30d'})
+    const token= jwt.sign({username,id},process.env.JWT_SECRET,{expiresIn: '30d'})
     // res.set('token','token')
-    console.log(token)
     res.status(201).json({msg:'user connected',token:token})
 }
 
