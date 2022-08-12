bd8d68f0a8071be5d5e9ff608e0aeb4e2a3c5380
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-12 04:04:57 +0200

    commit

--- controllers/main.js
+++ controllers/main.js
@@ -15,8 +15,19 @@ const login=async (req,res)=>{
 }
 
 const dashboard=async (req,res)=>{
-    const luckyNumber=Math.floor(Math.random()*100)
-    res.status(200).json({msg:'Hello, jess',secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
+    const authHeader = req.headers.authorization
+    if (!authHeader || !authHeader.startsWith('Bearer')){
+        throw new CustomAPIError('please provide your token', 401)
+    }
+    const token=authHeader.split(' ')[1]
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        const {username,id} = decoded
+        const luckyNumber=Math.floor(Math.random()*100)
+        res.status(200).json({msg:`Hello, ${username}`,secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
+    }catch (err){
+        throw new CustomAPIError('unauthorized connection :(', 401)
+    }
 }
 
 module.exports={login,dashboard}
